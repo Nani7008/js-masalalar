e2ac458cf679f514f3dbb5307d7458dce3d11b32
Nani7008 <narimonjon05@gmail.com>
2024-03-29 11:36:41 +0500

    homework

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// // 14. Uch xonali son berilgan. uning o'ngdan birinchi raqamini o`chirib chap tarafiga yozishdan hosil bo`lgan sonni aniqlovchi java script dasturida programma tuzilsin
+
+// let abc = +prompt ('abc=');
+// let a = (abc - abc%100)/100;
+// let b = (abc%100 - abc%10)/10;
+// let c = (abc%10)
+// alert(c*100+a*10+b)
+
+// 15.  Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan yuzliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin (Kirish=123 Chiqish 213)
+
+// let abc = +prompt ('abc=');
+// let a = (abc - abc%100)/100;
+// let b = (abc%100 - abc%10)/10;
+// let c = abc%10
+// alert(b*100+a*10+c)
+
+// 16. Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan birliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin (Kirish=123 Chiqish 132)
+
+// let abc = +prompt ('abc=');
+// let a = (abc - abc%100)/100;
+// let b = (abc%100 - abc%10)/10;
+// let c = abc%10
+// alert(a*100+c*10+b)
+
+// 17. 999 dan katta bo'lgan son berilgan. Bir marta bo'lib butunni va bo'lib qoldiqni olish operatsiyasidanfoydalanib berilgan sonni yuzliklar xonasidagi sonni aniqlovchi programma tuzilsin.
+// let abcd = +prompt ('abcd=');
+// alert((abcd%1000-abcd%100)/100);
+
+// 18.999 dan katta bo'lgan son berilgan. Bir marta bo'lib butunni va bo'lib qoldiqni olish operatsiyasidanfoydalanib berilgan sonni mingliklar xonasidagi sonni aniqlovchi programma tuzilsin.
+
+// let abcd = +prompt ('abcd=');
+// alert((abcd-abcd%1000)/1000);
+
+// 19. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha minut to’la o'tganligini aniqlovchi programma tuzilsin.
+
+// let sekund= +prompt ('sekund=')
+// alert(sekund/60 +" minut ")
+
+// 20. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha to’la soat o'tganligini aniqlovchi programma tuzilsin.
+
+// let sekund= +prompt ('sekund=')
+// alert(sekund/3600 +"soat")
+
+// 21. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha minut va sekund o’tganini aniqlovchi programma tuzilsin.
+// let sekund= +prompt ('sekund=')
+// let minut = (sekund / 60)
+// let s = sekund % 60;
+// alert(minut ,  s )
+
+// 22. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha soat va sekund o’tganini aniqlovchi programma tuzilsin.
+
+// let sekund= +prompt ('sekund=')
+// let soat = (sekund % 3600)
+// let s = (sekund-soat) / 3600;
+// alert(soat ,  s )
+
+
+// 23. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha soat, minut va sekund o’tganini aniqlovchi programma tuzilsin
+
+// let sekund= +prompt ('sekund=')
+// let soat = (sekund / 3600)
+// let s = sekund % 3600;
+// let minut = s/60
+// let sec= s%60
+// alert(soat + " soat " +  minut + " minut " +  sec + " soniya otdi " )
